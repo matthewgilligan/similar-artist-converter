3a82475d825a11784ede65c6d14a03dd9c853688
Matthew Gilligan <mcgilligan97@gmail.com>
2020-06-15 21:36:08 +0100

    rate limiting

--- converter.py
+++ converter.py
@@ -1,6 +1,8 @@
 import json
 import os
 import requests
+import requests_cache
+import time
 
 API_KEY = os.environ.get('LASTFM_API_KEY')
 USER_AGENT = 'Dataquest'
@@ -26,3 +28,34 @@ r = lastfm_get({'method': 'chart.gettopartists'})
 print(r.status_code)
 
 jprint(r.json()['artists']['@attr'])
+
+results = []
+
+page = 1
+total_pages = 99999
+
+while page <= total_pages:
+  payload = {
+      'method': 'chart.gettopartists',
+      'limit': 500,
+      'page': page
+  }
+
+  print("Requesting page {}/{}".format(page, total_pages))
+  clear_output(wait=True)
+
+  response = lastfm_get(payload)
+
+  if response.status_code != 200:
+    print(response.text)
+    break
+
+  page = int(response.json()['artists']['@attr']['page'])
+  total_page = int(response.json()['artists']['@attr']['totalPages'])
+
+  responses.append(response)
+
+  if not getattr(response, 'from_cache', False):
+    time.sleep(0.25)
+
+  page += 1
