b1b15686179e3b8b55e25db118bdc005faa56c08
Matthew Gilligan <mcgilligan97@gmail.com>
2020-06-15 21:21:04 +0100

    view the response

--- converter.py
+++ converter.py
@@ -1,19 +1,28 @@
+import json
 import os
 import requests
 
 API_KEY = os.environ.get('LASTFM_API_KEY')
 USER_AGENT = 'Dataquest'
 
-headers = {
-    'user-agent': USER_AGENT
-}
 
-payload = {
-    'api_key': API_KEY,
-    'method': 'chart.gettopartists',
-    'format': 'json'
-}
+def lastfm_get(payload):
+  headers = {'user-agent': USER_AGENT}
+  url = "http://ws.audioscrobbler.com/2.0/"
 
-r = requests.get('http://ws.audioscrobbler.com/2.0/',
-                 headers=headers, params=payload)
+  payload['api_key'] = API_KEY
+  payload['format'] = 'json'
+
+  response = requests.get(url, headers=headers, params=payload)
+  return response
+
+
+def jprint(obj):
+  text = json.dumps(obj, sort_keys=True, indent=4)
+  print(text)
+
+
+r = lastfm_get({'method': 'chart.gettopartists'})
 print(r.status_code)
+
+jprint(r.json()['artists']['@attr'])
