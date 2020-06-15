3dcb4c3473965bcef1bab55f80f320a8afda4dfd
Matthew Gilligan <mcgilligan97@gmail.com>
2020-06-15 21:07:47 +0100

    confirm successful request

--- converter.py
+++ converter.py
@@ -5,7 +5,15 @@ API_KEY = os.environ.get('LASTFM_API_KEY')
 USER_AGENT = 'Dataquest'
 
 headers = {
-    'user-agent': 'Dataquest'
+    'user-agent': USER_AGENT
 }
 
-r = requests.get('http://my-api-ulr', headers=headers)
+payload = {
+    'api_key': API_KEY,
+    'method': 'chart.gettopartists',
+    'format': 'json'
+}
+
+r = requests.get('http://ws.audioscrobbler.com/2.0/',
+                 headers=headers, params=payload)
+print(r.status_code)
